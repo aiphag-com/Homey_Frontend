4ff675bf32665be1f4ce6ca1360ceb229f637035
matiasgerstner <mgerstner@qourumit.com>
2022-05-30 19:03:43 -0300

    H-34 Fix: Fixed back button

--- src/Components/Forms/NewSellerForm.js
+++ src/Components/Forms/NewSellerForm.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import Icon from "@mdi/react";
-import { mdiBorderColor, mdiCheckCircle, mdiCloseCircle, mdiThumbUpOutline  } from "@mdi/js";
+import { mdiCheckCircle, mdiCloseCircle, mdiThumbUpOutline } from "@mdi/js";
 import { Link } from "react-router-dom";
 import { Button, Input } from "antd";
 
@@ -13,7 +13,7 @@ const NewSellerForm = () => {
     telefono: "",
     cvu: "",
   });
-  
+
   const updateForm = (e) => {
     setForm({
       ...form,
@@ -23,25 +23,23 @@ const NewSellerForm = () => {
 
   //the useState shows the form page
   const [count, setCount] = useState(1);
-  
-  const sum = ()=>{
-    setCount(count+1)
+
+  const sum = () => {
+    setCount(count + 1)
     console.log(count);
   }
 
-  const res = ()=>{
-    setCount(count-1)
+  const res = () => {
+    setCount(count - 1)
     console.log(count);
   }
 
   return (
     <div
-      id="seller_main"
       className="flex items-center justify-center align-middle h-screen bg-primaryI font-poppins leading-8"
       onSubmit={() => alert(`Submited thing: ${form.email}`)}
     >
       <div
-        id="seller_container"
         className="block items-center p-10  border-solid border-[2.5px] rounded-xl border-primaryI bg-[#ffffff]"
       >
         {count === 1 ? (
@@ -53,7 +51,7 @@ const NewSellerForm = () => {
 
             <p className="text-xl text-normal text-[#161616]"> CUIT </p>
             <div>
-              <Input                
+              <Input
                 placeholder="xx-xxxxxxx-x"
                 onChange={updateForm}
                 name="cuit"
@@ -169,7 +167,7 @@ const NewSellerForm = () => {
                 size={2}
               />
 
-              <p  className="text-xl text-normal text-[#161616]">
+              <p className="text-xl text-normal text-[#161616]">
                 {" "}
                 NOMBRE EMPRESA{" "}
               </p>
@@ -197,17 +195,17 @@ const NewSellerForm = () => {
           <div className="flex flex-col items-center justify-center">
 
             <Icon
-                id="ok"
-                className="text-[#71F7D7]"
-                path={mdiThumbUpOutline }
-                size={3}
-              />
+              id="ok"
+              className="text-[#71F7D7]"
+              path={mdiThumbUpOutline}
+              size={3}
+            />
 
             <div>
               <p >se ha generado una cuenta empresa para:</p>
             </div>
 
-            <p  className="text-3xl font-medium text-[#161616]">(Nombre empresa)</p>
+            <p className="text-3xl font-medium text-[#161616]">(Nombre empresa)</p>
 
             <div>
               <p>
@@ -220,44 +218,44 @@ const NewSellerForm = () => {
           </div>
         ) : null}
 
-        <div className="flex items-center justify-center">  
+        <div className="flex items-center justify-center">
           {/* if count is equal to 0, do not show "back" btn */}
           {count === 3 ? (
             <Link to="/AdminDashboard">
               <button
-              className="p-4 rounded-lg font-medium bg-[#ffffff]  text-[#8B11EF] border-2 border-[#8B11EF]">
-              Cargar otra empresa
+                className="p-4 rounded-lg font-medium bg-[#ffffff]  text-[#8B11EF] border-2 border-[#8B11EF]">
+                Cargar otra empresa
               </button>
             </Link>
           ) : (
             <>
-            <Button
-            onClick={()=>sum()}
-            type="default"
-            size="large"
-            className="mt-7"
-            style={{
-              background: "#8B11EF",
-              color: "#ffffff",
-              borderRadius: 7,
-            }}
-            >
-              {count === 1 ? "siguiente" : "generar usuario"}
-            </Button>
-
-            <Button
-            onClick={()=>res()}
-            type="default"
-            size="large"
-            className="mt-7 ml-5"
-            style={{
-              background: "#ffffff",
-              borderRadius: 7,
-            }}
-            >
-              atras
-            </Button>
-
+              <Button
+                onClick={() => sum()}
+                type="default"
+                size="large"
+                className="mt-7"
+                style={{
+                  background: "#8B11EF",
+                  color: "#ffffff",
+                  borderRadius: 7,
+                }}
+              >
+                {count === 1 ? "siguiente" : "generar usuario"}
+              </Button>
+              {count !== 1 ? 
+              (<Button
+                onClick={() => res()}
+                type="default"
+                size="large"
+                className="mt-7 ml-5"
+                style={{
+                  background: "#ffffff",
+                  borderRadius: 7,
+                }}
+              >
+                atras
+              </Button>) 
+              : null}
             </>
           )}
         </div>
