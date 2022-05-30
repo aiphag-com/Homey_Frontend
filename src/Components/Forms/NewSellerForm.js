95de1064369655dfd5bc938a8e372c6dad9c7a76
matiasgerstner <mgerstner@qourumit.com>
2022-05-30 20:18:48 -0300

    Fix: Added validations

--- src/Components/Forms/NewSellerForm.js
+++ src/Components/Forms/NewSellerForm.js
@@ -1,8 +1,9 @@
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
 import Icon from "@mdi/react";
 import { mdiCheckCircle, mdiCloseCircle, mdiThumbUpOutline } from "@mdi/js";
 import { Link } from "react-router-dom";
 import { Button, Input } from "antd";
+import { emailRegexValidation } from "../../Utils/Utils";
 
 const NewSellerForm = () => {
   const [form, setForm] = useState({
@@ -13,7 +14,9 @@ const NewSellerForm = () => {
     telefono: "",
     cvu: "",
   });
-
+  const [errorsData, setErrorsData] = useState(null);
+  //the useState shows the form page
+  const [count, setCount] = useState(1);
   const updateForm = (e) => {
     setForm({
       ...form,
@@ -21,27 +24,60 @@ const NewSellerForm = () => {
     });
   };
 
-  //the useState shows the form page
-  const [count, setCount] = useState(1);
+
 
   const sum = () => {
     setCount(count + 1)
-    console.log(count);
   }
 
   const res = () => {
     setCount(count - 1)
-    console.log(count);
   }
 
+  const validateErrors = useCallback(() => {
+    const errors = {};
+    let isError = false;
+
+    if (!form?.cuit || form?.cuit.length !== 11) {
+      errors['cuit'] = true;
+      isError = true;
+    }
+    if (!form?.direccion || form?.direccion.length < 10) {
+      errors['direccion'] = true;
+      isError = true;
+    }
+    if (!form?.cp || form?.cp.length !== 4) {
+      errors['cp'] = true;
+      isError = true;
+    }
+    if (!form?.telefono || form?.telefono.length !== 10) {
+      errors['telefono'] = true;
+      isError = true;
+    }
+    if (!form?.email || form?.email.length < 10
+      || emailRegexValidation.test(form?.email) === false) {
+      errors['email'] = true;
+      isError = true;
+    }
+
+    return {
+      isError, errors,
+    };
+  }, [form]);
+
+  const handleNextStep = () => {
+    const { isError, errors } = validateErrors();
+    if (isError) {
+      setErrorsData(errors);
+    } else {
+      setErrorsData(null);
+      sum();
+    }
+  };
+
   return (
-    <div
-      className="flex items-center justify-center align-middle h-screen bg-primaryI font-poppins leading-8"
-      onSubmit={() => alert(`Submited thing: ${form.email}`)}
-    >
-      <div
-        className="block items-center p-10  border-solid border-[2.5px] rounded-xl border-primaryI bg-[#ffffff]"
-      >
+    <div className="flex items-center justify-center align-middle h-screen bg-primaryI font-poppins leading-8 h-full w-full">
+      <div className="block items-center p-10  border-solid border-[2.5px] rounded-xl border-primaryI bg-[#ffffff]">
         {count === 1 ? (
           <div>
             <p className="text-xl text-semibold text-[#838383]">
@@ -50,29 +86,27 @@ const NewSellerForm = () => {
             <p className="text-4xl font-bold">1. Carga datos de la empresa</p>
 
             <p className="text-xl text-normal text-[#161616]"> CUIT </p>
-            <div>
+            <div className="flex flex-col">
               <Input
+                name="cuit"
+                type="number"
                 placeholder="xx-xxxxxxx-x"
                 onChange={updateForm}
-                name="cuit"
-                value={form.cuit}>
-              </Input>
+                value={form.cuit}
+                style={{width: '44%' }}
+                />
+              {errorsData?.cuit ? <p>El numero de CUIT debe tener 11 digitos y solo se permiten numeros</p>: null}
             </div>
             <br />
 
             <p className="text-xl text-normal text-[#161616]">
-              {" "}
               Domicilio de facturacion
-              {" "}
             </p>
 
             <div className="flex">
-
               <div className="mr-10">
                 <p className="text-sm text-normal text-[#161616]">
-                  {" "}
                   Direccion
-                  {" "}
                 </p>
                 <div>
                   <Input
@@ -81,22 +115,23 @@ const NewSellerForm = () => {
                     name="direccion"
                     value={form.direccion}
                   />
+                 {errorsData?.direccion ? <p>La direccion debe tener como minimo 10 caracteres</p> : null}
                 </div>
               </div>
 
               <div className="mr-10">
                 <p className="text-sm text-normal text-[#161616]">
-                  {" "}
                   CP
-                  {" "}
                 </p>
                 <div>
                   <Input
+                    type="number"
                     placeholder="xxxx"
                     onChange={updateForm}
                     name="cp"
                     value={form.cp}
                   />
+                {errorsData?.cp ? <p>El codigo postal es de 4 numeros</p> : null}
                 </div>
               </div>
 
@@ -104,9 +139,7 @@ const NewSellerForm = () => {
 
             <br />
             <p className="text-xl text-normal text-[#161616]">
-              {" "}
               Mail de contacto
-              {" "}
             </p>
             <div>
               <Input
@@ -115,19 +148,20 @@ const NewSellerForm = () => {
                 name="email"
                 value={form.email}
               />
+            {errorsData?.email ? <p>El correo electronico debe contener @ y .com</p> : null}
             </div>
 
             <br />
             <p className="text-xl text-normal text-[#161616]"> Telefono </p>
             <div>
               <Input
-                type="tel"
-                pattern="[0-9] {5}"
+                type="number"
                 placeholder="xx-xxx-xx-xxxxxxx"
                 onChange={updateForm}
                 name="telefono"
                 value={form.telefono}
               />
+           {errorsData?.telefono ? <p>El telefono debe contener 10 numeros</p> : null}
             </div>
           </div>
         ) : null}
@@ -150,6 +184,7 @@ const NewSellerForm = () => {
                 name="cvu"
                 value={form.cvu}
               />
+            {errorsData?.cvu ? <p>El C.V.U debe contener 22 numeros</p> : null}
             </div>
 
             <div className="flex flex-col items-center justify-center mb-10 mt-10">
@@ -168,23 +203,20 @@ const NewSellerForm = () => {
               />
 
               <p className="text-xl text-normal text-[#161616]">
-                {" "}
-                NOMBRE EMPRESA{" "}
+                NOMBRE EMPRESA
               </p>
 
               <div className="flex">
                 <p className="text-normal  text-[#161616]"> CUIT/CUIL: </p>
                 <p className=" ml-2 text-l  text-normal  text-[#161616]">
-                  {" "}
-                  999.999.999{" "}
+                  999.999.999
                 </p>
               </div>
 
               <div className="flex">
                 <p className="text-normal  text-[#161616]"> ALIAS: </p>
                 <p className=" ml-2 text-l  text-normal  text-[#161616]">
-                  {" "}
-                  alias name{" "}
+                  alias name
                 </p>
               </div>
             </div>
@@ -230,7 +262,7 @@ const NewSellerForm = () => {
           ) : (
             <>
               <Button
-                onClick={() => sum()}
+                onClick={handleNextStep}
                 type="default"
                 size="large"
                 className="mt-7"
@@ -242,20 +274,20 @@ const NewSellerForm = () => {
               >
                 {count === 1 ? "siguiente" : "generar usuario"}
               </Button>
-              {count !== 1 ? 
-              (<Button
-                onClick={() => res()}
-                type="default"
-                size="large"
-                className="mt-7 ml-5"
-                style={{
-                  background: "#ffffff",
-                  borderRadius: 7,
-                }}
-              >
-                atras
-              </Button>) 
-              : null}
+              {count !== 1 ?
+                (<Button
+                  onClick={() => res()}
+                  type="default"
+                  size="large"
+                  className="mt-7 ml-5"
+                  style={{
+                    background: "#ffffff",
+                    borderRadius: 7,
+                  }}
+                >
+                  atras
+                </Button>)
+                : null}
             </>
           )}
         </div>
