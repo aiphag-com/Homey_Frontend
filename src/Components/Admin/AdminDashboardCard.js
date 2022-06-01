5f15ab1c8b63c3b447b7f73497e0aceae00630a4
matiasgerstner <mgerstner@qourumit.com>
2022-06-01 12:09:43 -0300

    H-29 fix: fix links to menu and admin dashboard

--- src/Components/Admin/AdminDashboardCard.js
+++ src/Components/Admin/AdminDashboardCard.js
@@ -1,16 +1,15 @@
 import React from "react";
 import image from '../../Assets/PlaceHolderCardAdmin.png';
 
-// aca tiene que venir con props del componente padre
-const AdminDashboardCard = () => {
+const AdminDashboardCard = (props) => {
 
   return (
     <>
-      <div class="max-w-sm bg-white rounded-lg border border-gray-200 shadow-md">
-        <img class="rounded-t-lg" src={image} alt="" />
-        <div class="p-5">
-          <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">name</h5>
-          <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">description</p>
+      <div className="max-w-sm bg-white rounded-lg border border-gray-200 shadow-md">
+        <img className="rounded-t-lg" src={image} alt="" />
+        <div className="p-5">
+          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{props.name}</h5>
+          <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{props.description}</p>
         </div>
       </div>
     </>
