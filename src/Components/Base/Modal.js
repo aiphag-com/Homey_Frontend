40b2b63c426a4ff38735ee33aa3c928189326bad
EliasCorrea <elias@aiphag.com>
2022-05-31 22:03:12 -0300

    Modal Style
    https://aiphag.atlassian.net/jira/software/projects/HOMEY/boards/3?selectedIssue=HOMEY-29

--- src/Components/Base/Modal.js
+++ src/Components/Base/Modal.js
@@ -13,6 +13,14 @@ export const LocalizedModal = () => {
     setVisible(false);
   };
 
+  const handleOk = () => {
+    hideModal();
+  };
+
+  const handleCancel = () => {
+    hideModal();
+  };
+
   return (
     <>
       <Button type="primary" onClick={showModal}>
@@ -22,17 +30,45 @@ export const LocalizedModal = () => {
         visible={visible}
         onOk={hideModal}
         onCancel={hideModal}
-        okText="Habilitar"
-        cancelText="Pausar"
+        footer={[
+          <div className='flex justify-center'>
+            <Button 
+            onClick={handleCancel} 
+            danger
+            style={{borderRadius: 7, borderColor: "red", color: "red"}} 
+            > Pausar </Button>
+
+            <Button 
+            onClick={handleOk} 
+            style={{borderRadius: 7, borderColor: "green", color: "green"}}
+            > Habilitar </Button>
+          </div>
+        ]}
       >
         <div className="grid grid-cols-2">
           <div>
-          aca va todo el lado izquierdo el texto
+            <a href='/error' name="Manufacturer"> Empresa </a>
+            <p name="ProductName" className='font-bold'>NOMBRE DEL PRODUCTO </p>
+            <p name="ProuctDescription"> descripcion del producto </p>
+            <p className='font-bold'>MEDIDAS</p>
+            <p name="ProductDimentions"> 999cm x 999cm </p>
           </div>
           <div className='mt-10'>
             <CarouselComponent />
-            <Button type="primary" >
-            Descargar modelo 3D
+
+            <Button
+              type="primary"
+              size="large"
+              block
+              style={{
+                background: "#9254DE",
+                height: 50,
+                color: "white",
+                borderRadius: 7,
+                borderColor: "transparent",
+              }}
+            >
+              Descargar Modelo 3D
             </Button>
           </div>
         </div>
