9238784a31677fec59a66590a522dfa2814a7b50
EliasCorrea <elias@aiphag.com>
2022-06-01 23:01:40 -0300

    admin dashboard menu ordered
    https://aiphag.atlassian.net/browse/HOMEY-32?focusedCommentId=10067

--- src/Components/Admin/Dashboard.js
+++ src/Components/Admin/Dashboard.js
@@ -1,20 +1,26 @@
 import React from "react";
 import AdminDashboardCard from "./AdminDashboardCard";
-import {data} from '../../Utils/Utils';
+import { data } from "../../Utils/Utils";
 import { Link } from "react-router-dom";
 
 const AdminDashboard = () => {
-    return (
-        <>
-            <div className="grid grid-cols-2 gap-4 font-poppins ">
-          {data.map(data => (
-              <Link to={data.link}>
-              <AdminDashboardCard key={data.id} name={data?.name} description={data.description} />
-              </Link>
-              ))}
-            </div>
-        </>
-    );
-}
+  return (
+    <>
+    <div className="flex justify-center items-center font-poppins h-100 w-screen">
+      <div className="grid grid-cols-3 gap-10 p-5">
+        {data.map((data) => (
+          <Link to={data.link}>
+            <AdminDashboardCard
+              key={data.id}
+              name={data?.name}
+              description={data.description}
+            />
+          </Link>
+        ))}
+      </div>
+    </div>
+    </>
+  );
+};
 
 export default AdminDashboard;
